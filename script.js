b3bc6f8265803cd004e05b1ecb9086a9a9e61f3d
matthewcheng13 <mcheng.mm@gmail.com>
2023-10-14 19:47:40 -0400

    Improved formatting and updated descriptions

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
-// Wait for the document to load before running the script 
-(function ($) {
-  
-  // We use some Javascript and the URL #fragment to hide/show different parts of the page
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
-  $(window).on('load hashchange', function(){
-    
-    // First hide all content regions, then show the content-region specified in the URL hash 
-    // (or if no hash URL is found, default to first menu item)
-    $('.content-region').hide();
-    
-    // Remove any active classes on the main-menu
-    $('.main-menu a').removeClass('active');
-    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-    
-    // Now show the region specified in the URL hash
-    $(region).show();
-    
-    // Highlight the menu link associated with this region by adding the .active CSS class
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
-
-    // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
-    // This will extract the region name from URL hash, and then load [region].html into the main #content div
-    // var region = location.hash.toString() || '#first';
-    // $('#content').load(region.slice(1) + '.html')
-    
-  });
-  
-})(jQuery);
-
-function showDropdownContent(clicked, toShow) {
-  let button = document.getElementById(clicked);
-  let menu = document.getElementById(toShow)
-
-  if (menu.style.display === "none") {
-    menu.style.display = "inline";
-    button.innerHTML = "&#9650;";
-  } else {
-    menu.style.display = "none";
-    button.innerHTML = "&#9660;";
+$(document).ready(function() {
+  // Function to handle showing/hiding content regions and menu highlighting
+  function handleContent() {
+      $('.content-region').hide();
+      $('.main-menu a').removeClass('active');
+      const region = location.hash.toString() || $('.main-menu a:first').attr('href');
+      $(region).show();
+      $('.main-menu a[href="'+ region +'"]').addClass('active'); 
   }
-}
+
+  // Event listener for hashchange
+  $(window).on('load hashchange', function() {
+      handleContent();
+  });
+
+  // Event listener for dropdown button click
+  $('.show-button').click(function() {
+      const buttonId = this.id;
+      const contentId = buttonId.replace('_show', '_content');
+      const menu = document.getElementById(contentId);
+
+      if (menu.style.display === "none") {
+          menu.style.display = "inline";
+          this.innerHTML = "&#9650;";
+      } else {
+          menu.style.display = "none";
+          this.innerHTML = "&#9660;";
+      }
+  });
+});
