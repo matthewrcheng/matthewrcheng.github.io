5037f0887854763bbe926a09795d92d04565328d
mcheng <mcheng@sitscape.com>
2024-05-28 01:01:09 -0400

    Redesigned site

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,59 @@
-$(document).ready(function() {
-  // Function to handle showing/hiding content regions and menu highlighting
-  function handleContent() {
-      $('.content-region').hide();
-      $('.main-menu a').removeClass('active');
-      const region = location.hash.toString() || $('.main-menu a:first').attr('href');
-      $(region).show();
-      $('.main-menu a[href="'+ region +'"]').addClass('active'); 
-  }
-
-  // Event listener for hashchange
-  $(window).on('load hashchange', function() {
-      handleContent();
-  });
-
-  // Event listener for dropdown button click
-  $('.show-button').click(function() {
-      const buttonId = this.id;
-      const contentId = buttonId.replace('_show', '_content');
-      const menu = document.getElementById(contentId);
-
-      if (menu.style.display === "none") {
-          menu.style.display = "inline";
-          this.innerHTML = "&#9650;";
-      } else {
-          menu.style.display = "none";
-          this.innerHTML = "&#9660;";
-      }
-  });
-
-  // Event listener for form submission
-  $('#message-form').submit(function(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-
-    // Get form data
-    const name = $('#name').val();
-    const email = $('#email').val();
-    const message = $('#message').val();
-    
-    emailjs.init(key)
-
-    // Use EmailJS to send the email
-    emailjs.send(service, template, {
-      from_name: name,
-      from_email: email,
-      message: message,
-    }).then(function(response) {
-      console.log("Email sent successfully:", response);
-      // You can add code here to show a success message to the user
-    }, function(error) {
-      console.error("Email sending failed:", error);
-      // You can add code here to show an error message to the user
-    });
-
-    // Clear the form fields
-    $('#name').val('');
-    $('#email').val('');
-    $('#message').val('');
+document.querySelectorAll('.vertical-nav ul li a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  toggleMode('auto');
+});
+
+document.addEventListener('scroll', (event) => {
+  const sections = document.querySelectorAll(".section");
+  const navLinks = document.querySelectorAll(".vertical-nav ul li a");
+  let currentSection = "";
+  console.log("sad");
+
+  sections.forEach(section => {
+    console.log("bye");
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (window.scrollY >= sectionTop - sectionHeight / 3) {
+      currentSection = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    console.log(currentSection);
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(currentSection)) {
+      console.log("hi");
+      link.classList.add("active");
+    }
+  });
+});
+
+function toggleMode(mode) {
+  var modeIcon = document.getElementById('modeIcon');
+  if (mode === 'light') {
+      document.body.classList.remove('dark-mode');
+      modeIcon.classList.remove('fa-moon', 'fa-circle-half-stroke', 'dark-toggle');
+      modeIcon.classList.add('fa-sun');
+  } else if (mode === 'dark') {
+      document.body.classList.add('dark-mode');
+      modeIcon.classList.remove('fa-sun', 'fa-circle-half-stroke');
+      modeIcon.classList.add('fa-moon', 'dark-toggle');
+  } else {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        toggleMode('dark');
+      } else {
+        toggleMode('light')
+      }
+      modeIcon.classList.remove('fa-sun', 'fa-moon');
+      modeIcon.classList.add('fa-circle-half-stroke');
+  }
+}
